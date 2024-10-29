bda3b499cc15f70485a343c438823018ca89fa78
Crispykitty <u22536800@tuks.co.za>
2024-10-29 23:43:32 +0200

    re-added industrial factories

---
+++ CityBuilderSimulator/src/City/CityComponent/Industrial/MetalWorkFacility.h
@@ -0,0 +1,14 @@
+#ifndef METALWORKFACILITY_H
+#define METALWORKFACILITY_H
+
+#include "Industry.h"
+
+// Concrete Industry: Metal Factory
+class MetalWorkFacility : public Industry {
+public:
+    MetalWorkFacility();
+    MetalWorkFacility(std::shared_ptr<IncomeResourceProduct> gold, 
+                 std::shared_ptr<ConstructionResourceProduct> steel);
+};
+
+#endif  // METALWORKS_H

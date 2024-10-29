49255700cbb14ebd258f7f91eccc6c1bd88455b8
RNkwana <retshepilenkwana@gmail.com>
2024-10-29 12:37:08 +0200

    PublicService h files

---
+++ CityBuilderSimulator/src/City/CityComponent/PublicService/Hospital.h
@@ -0,0 +1,21 @@
+#ifndef HOSPITAL_H
+#define HOSPITAL_H
+
+#include "PublicService.h"
+
+class Hospital : public PublicService  {
+private:
+    bool water;
+    bool electricity;
+    bool sewage;
+    bool wasteManagement;
+    std::string status;
+public:
+    Hospital(bool waterStatus, bool electricityStatus, bool wasteManagementStatus, bool sewageStatus, std::string buildingStatus);
+    ~Hospital(){};
+    void provideService();
+    std::unique_ptr<PublicService> clone() const;
+
+};
+
+#endif

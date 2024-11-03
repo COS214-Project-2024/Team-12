60df6e1c14d938566af7d00422b0a388a4a46ef5
OkageShadow7 <u22591380@tuks.co.za>
2024-11-03 19:14:48 +0200

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/sibus/OneDrive/Documents/GitHub/Team-12
-# Build directory: /mnt/c/Users/sibus/OneDrive/Documents/GitHub/Team-12/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(runTests "/mnt/c/Users/sibus/OneDrive/Documents/GitHub/Team-12/build/runTests")
-set_tests_properties(runTests PROPERTIES  _BACKTRACE_TRIPLES "/mnt/c/Users/sibus/OneDrive/Documents/GitHub/Team-12/CMakeLists.txt;81;add_test;/mnt/c/Users/sibus/OneDrive/Documents/GitHub/Team-12/CMakeLists.txt;0;")
-subdirs("gtests")

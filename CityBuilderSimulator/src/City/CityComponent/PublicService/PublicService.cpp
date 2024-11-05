7167f1c34422755bc3daaa3174bebb68389f0000
RNkwana <retshepilenkwana@gmail.com>
2024-11-05 07:47:32 +0200

    Updated cpp file(ps)

--- CityBuilderSimulator/src/City/CityComponent/PublicService/PublicService.cpp
+++ CityBuilderSimulator/src/City/CityComponent/PublicService/PublicService.cpp
@@ -1,6 +1,6 @@
 #include "PublicService.h"
 #include <iostream>
-
+//cpp file new
 PublicService::PublicService(bool waterStatus, bool electricityStatus, bool wasteManagementStatus, bool sewageStatus, std::string buildingStatus){
 this->electricity = electricityStatus;
 this->water = waterStatus;
